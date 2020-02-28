eb89a8d6fcf9e2393afdf6669fbc2b89d1818aba
ShomiKhan <nkskl6@gmail.com>
2020-02-28 14:57:44 +0600

    selis map  string updated

--- selis/map and string.cpp
+++ selis/map and string.cpp
@@ -3,22 +3,32 @@ using namespace std;
 
 int main()
 {
+
     map<string, long>mp;
-    mp["asdf"] = 1;
-    mp["qwer"] = 2;
+    mp["aa"] = 1;
+    mp["aa"] = 2;
 
     map<string, long>:: iterator it;
-    for(it=mp.begin() ; it != mp.end() ; it++)
+    for(it=mp.begin() ; it!=mp.end() ; it++)
         cout<<it->first << " "<< it->second<<endl;
-    
+
     string a = mp.begin()->first;
     cout<<a<<"   len = "<<a.size()<<endl;
-    a = "";
+    a[a.size()-1] = 'Z';
     cout<<a<<"   len = "<<a.size()<<endl;
 
     printf("Input a string: ");
     getline(cin, a);
     cout<<a<<endl;
 
+
+
+    string aa = "asdf", ss = "Zsd";
+    if(aa > ss)
+        printf("HaHa\n");
+    else if(aa < ss)
+        printf("HiHi\n");
+    else if(aa == ss)
+        printf("HoHo\n");
     return 0;
 }
