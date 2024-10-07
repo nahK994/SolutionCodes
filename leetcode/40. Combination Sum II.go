7f087e1702082f3b78e8ca6954f458abc3b180f9
nahK994 <nkskl6@gmail.com>
2024-10-08 01:12:45 +0600

    solve leetcode 40

--- leetcode/40. Combination Sum II.go
+++ leetcode/40. Combination Sum II.go
@@ -1,17 +1,14 @@
-package main
+package random
 
 import (
-	"fmt"
 	"sort"
 )
 
-var ans [][]int
-
-func rec(candidates []int, target int, idx int, list []int, sum int) {
+func rec(ans *[][]int, candidates []int, target int, idx int, list []int, sum int) {
 	if sum == target {
 		temp := make([]int, len(list))
 		copy(temp, list)
-		ans = append(ans, temp)
+		*ans = append(*ans, temp)
 		return
 	} else if sum > target || idx == len(candidates) {
 		return
@@ -22,28 +19,19 @@ func rec(candidates []int, target int, idx int, list []int, sum int) {
 			continue
 		}
 
-		rec(candidates, target, i+1, list, sum)
+		rec(ans, candidates, target, i+1, list, sum)
 		break
 	}
 
 	list = append(list, candidates[idx])
-	rec(candidates, target, idx+1, list, sum+candidates[idx])
+	rec(ans, candidates, target, idx+1, list, sum+candidates[idx])
 }
 
 func combinationSum2(candidates []int, target int) [][]int {
 	sort.Slice(candidates, func(i, j int) bool {
 		return candidates[i] < candidates[j]
 	})
-
-	rec(candidates, target, 0, []int{}, 0)
+	var ans [][]int
+	rec(&ans, candidates, target, 0, []int{}, 0)
 	return ans
-}
-
-func main() {
-	// combinationSum2([]int{10, 1, 2, 7, 6, 1, 5}, 8)
-	// combinationSum2([]int{2, 5, 2, 1, 2}, 5)
-	combinationSum2([]int{4, 4, 4, 4}, 8)
-	for _, item := range ans {
-		fmt.Println(item)
-	}
 }
